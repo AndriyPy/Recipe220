4f74d9e2b2b6ae576d8d3daf57d38e20b81ec59a
Andriyko222 <125126827+Andriyko222@users.noreply.github.com>
2026-02-10 23:55:04 +0200

    test ai

--- project/AI_assistent/views.py
+++ project/AI_assistent/views.py
@@ -3,7 +3,7 @@ from openrouter import OpenRouter
 import os
 
 def recipe_ai_view(request):
-    ingredients = request.GET.get("ingredients", "")
+    ingredients = request.GET.get("grass, cucumber, carrot, beef, ketchup, potato")
 
     with OpenRouter("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") as client:
         response = client.chat.send(
