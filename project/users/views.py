0cb2d68f0a7c446d07747b19f7f5de9ba80bc983
Eva <evacherkashyna9@gmail.com>
2026-02-10 23:08:49 +0200

    new register

--- project/users/views.py
+++ project/users/views.py
@@ -1,19 +1,51 @@
 from django.shortcuts import render, redirect
-from django.http.request import HttpRequest
-from django.contrib.auth import authenticate, login, logout
-from .forms import UserRegisterForm, UserLoginForm
-from django.http.response import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils import timezone
+import random
+from datetime import timedelta
+from django.core.mail import EmailMessage, send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
-
+from .forms import UserRegisterForm, UserLoginForm, EmailVerificationForm
+from .models import Profile, EmailVerification
 
 def register_view(request: HttpRequest):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            code = str(random.randint(100000, 999999))
+
+            verification = EmailVerification.objects.create(
+                user=user,
+                code=code,
+                expires_at=timezone.now() + timedelta(minutes=10)
+            )
+
+            email_message = EmailMessage(
+                subject='Email confirmation',
+                body=render_to_string(
+                    'mailing/email_verification.html',
+                    context={
+                        'code': code,
+                        'user': user,
+                    }
+                ),
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[user.email],
+            )
+            email_message.content_subtype = 'html'
+            email_message.send(fail_silently=False)
+
+            request.session['verify_user_id'] = user.id
+            return redirect('verify_email')
 
     else:
         form = UserRegisterForm()
