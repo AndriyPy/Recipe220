ccd18f9a17b593944f0c6d0492ee435bcbe33a71
Andriyko222 <125126827+Andriyko222@users.noreply.github.com>
2026-02-13 13:50:14 +0200

    add form for ai

--- project/AI_assistent/views.py
+++ project/AI_assistent/views.py
@@ -1,17 +1,23 @@
+
 from django.shortcuts import render
 from openrouter import OpenRouter
 import os
 
 def recipe_ai_view(request):
-    ingredients = request.GET.get("grass, cucumber, carrot, beef, ketchup, potato")
+    recipe = None
+    ingredients = request.GET.get("ingredients")
 
-    with OpenRouter("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") as client:
-        response = client.chat.send(
-            model="minimax/minimax-m2",
-            messages=[{"role": "user", "content": f"Create a recipe with: {ingredients}"}],
-            max_tokens=4000
-        )
+    if ingredients:
+        with OpenRouter(api_key="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") as client:
+            response = client.chat.send(
+                model="mistralai/mistral-7b-instruct",
+                messages=[
+                    {"role": "user", "content": f"Create a simple recipe using: {ingredients}"}
+                ],
+                max_tokens=500
+            )
+            recipe = response.choices[0].message.content
 
-        text = response.choices[0].message.content
+    return render(request, "ai/recipe_ai.html", {"recipe": recipe})
 
-    return render(request, "ai/recipe_ai.html", {"recipe": text})
+
