73d8e76d2fe02f7c876c5e912ca93aa0632a7314
Eva <evacherkashyna9@gmail.com>
2026-03-02 18:03:18 +0200

    new admin (ai_assistent)

--- project/AI_assistent/admin.py
+++ project/AI_assistent/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipes
 
-# Register your models here.
+
+@admin.register(Recipes)
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['created_at']
+    search_fields = ['title', 'ingredients', 'user__username']
+    readonly_fields = ['created_at']
+    ordering = ['-created_at']4
+    list_per_page = 25
